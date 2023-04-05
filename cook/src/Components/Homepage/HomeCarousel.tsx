03c4f53c90cc991b014aa3f98a89e8d9a5dbb547
DanDecoito <116022076+DanDecoito@users.noreply.github.com>
2023-04-05 14:32:20 -0700

    For Safety

---
+++ cook/src/Components/Homepage/HomeCarousel.tsx
@@ -0,0 +1,18 @@
+
+import img1 from "../../Assets/Unknown.jpeg"
+
+interface CarouselProps {
+
+}
+
+const HomeCarousel: React.FC<CarouselProps> = ({ }) => {
+    return (
+      <>
+        
+        
+
+      </>
+    );
+  };
+  
+  export default HomeCarousel;
