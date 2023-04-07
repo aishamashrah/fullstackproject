eca269192b642ad902f4b8b52e1cd430d5ee87ab
aisha mashrah <aishamashrah@gmail.com>
2023-04-07 12:48:54 -0700

    footer done

--- cook/src/App.tsx
+++ cook/src/App.tsx
@@ -9,7 +9,7 @@ import Dashboard from './Components/Dashboard/Dashboard';
 function App() {
   return (
     <div className="">
-        <Articles/>
+        <CookEaseFooter/>
      
     </div>
   );
--- cook/src/Components/Footer/Footer.tsx
+++ cook/src/Components/Footer/Footer.tsx
@@ -1,41 +1,53 @@
-import chefHat from '../../Assets/Images/chef.png'
-import facebook from '../../Assets/Images/facebook.png'
-import pinterest from '../../Assets/Images/pinterest.png'
-import twitter from '../../Assets/Images/twitter.png'
-import instagram from '../../Assets/Images/instagram.png'
-
+import chefHat from '../../Assets/chef.png'
+import facebook from '../../Assets/facebook.png'
+import pinterest from '../../Assets/pinterest.png'
+import twitter from '../../Assets/twitter.png'
+import instagram from '../../Assets/instagram.png'
+import cookpic from '../../Assets/cooking.png'
 export default function CookEaseFooter() {
 
     return (
-        <>
-            <div className="flex bg-slate-400 w-full h-36">
-                <div className="flex ml-8">
-                    <button>
-                        <img className='h-16 mb-12 rotate-10' src={chefHat} />
-                    </button>
-                    <div>
-                        <p className='font-lobster mt-8 ml-3 text-4xl'>Cook Ease</p>
-                        <div className='flex'>
-                            <img className='h-8 ml-3 ' src={facebook} />
-                            <img className='h-8 ml-2 ' src={pinterest} />
-                            <img className='h-8 ml-2 ' src={twitter} />
-                            <img className='h-8 ml-2 ' src={instagram} />
-                        </div>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4 footerbg w-full md:h-36">
+            <div className="flex  justify-center md:justify-center md:ml-8  mb-10">
+                <button>
+                    <img className="h-16 mb-2 md:mb-0 md:rotate-10" src={chefHat} alt="Chef Hat" />
+                </button>
+                <div className="text-center md:text-left lg:p-7">
+                    <p className="font-lobster text-2xl md:text-4xl">Cook Ease</p>
+                    <div className="flex justify-center md:justify-start">
+                        <img className="h-6 md:h-8 mx-1" src={facebook} alt="Facebook" />
+                        <img className="h-6 md:h-8 mx-1" src={pinterest} alt="Pinterest" />
+                        <img className="h-6 md:h-8 mx-1" src={twitter} alt="Twitter" />
+                        <img className="h-6 md:h-8 mx-1" src={instagram} alt="Instagram" />
                     </div>
                 </div>
-                <div>
-                    <p>test</p>
-                    <p>test</p>
-                    <p>test</p>
-                    <p>test</p>
-                </div>
-     
-
-
-
-
             </div>
 
-        </>
+            <div className="lg:p-4">
+                <div>
+                    <p className="text-center md:text-left">Search</p>
+                    <p className="text-center md:text-left">News</p>
+                    <p className="text-center md:text-left">Kitchen Tips</p>
+                    <p className="text-center md:text-left">About Us</p>
+                </div>
+            </div>
+
+            <div className="lg:p-1 flex  justify-center md:justify-center md:ml-8  ">
+                <button>
+                    <img className="h-16 mb-2 md:mb-0 md:h-24 md:rotate-10" src={cookpic} alt="Cooking Picture" />
+                </button>
+            </div>
+
+            <div className="lg:p-4">
+                <div>
+                    <p className="text-center md:text-left">Contact Us</p>
+                    <p className="text-center md:text-left">Community Guidelines</p>
+                    <p className="text-center md:text-left">Site Map</p>
+                    <p className="text-center md:text-left">&copy; 2023 DanIsCool, LLC</p>
+                </div>
+            </div>
+        </div>
+
+
     )
 }
